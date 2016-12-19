d3c04d420c27cbf2966ba4e5168b3a419c7bd46c
Annie Sonna <nsab42@yahoo.com>
2016-12-18 22:45:28 -0800

    add to more urls enabling entry and saving of new client and stylist.

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -23,11 +23,24 @@ namespace HairSalon
       };
 
       Get["/clients/new"] = _ => {
-        return View["clients_form.cshtml"];
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["clients_form.cshtml", AllStylists];
       };
 
       Get["/stylists/new"] = _ => {
         return View["stylists_form.cshtml"];
+      };
+
+      Post["/stylists/new"] = _ => {
+        Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
+        newStylist.Save();
+        return View["success.cshtml"];
+      };
+
+      Post["/clients/new"] = _ => {
+        Client newClient = new Client(Request.Form["client-name"], Request.Form["stylistId"]);
+        newClient.Save();
+        return View["success.cshtml"];
       };
 
     }
